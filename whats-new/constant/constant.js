55fd6dc80235c492b7da92d067d3a6e177f43307
recallhyx <quce.hu@qq.com>
2018-04-17 11:39:38 +0800

    抽出查询语句

--- whats-new/constant/constant.js
+++ whats-new/constant/constant.js
@@ -1,6 +1,61 @@
 export const CONSTANT_URL = "https://api.github.com/graphql";
 // 填入自己的 Github Access_Token
 export const CONSTANT_TOKEN = "";
+export const CONSTANT_QueryInfoWithOwner = function (owner,num){
+  return`query {
+      repositoryOwner(login: "${owner}") {
+      avatarUrl
+      url
+      repositories(orderBy: {field: STARGAZERS, direction: DESC}, first: ${num}) {
+        totalCount
+        edges {
+          node {
+            name
+            stargazers {
+              totalCount
+            }
+            releases(last: 1) {
+              edges {
+                node {
+                  tag {
+                    name
+                  }
+                  url
+                  updatedAt
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+};
+export const CONSTANT_QueryInfoRepository = function (owner,repository){
+  return`query {
+      repository(owner:"${owner}",name:"${repository}"){
+        owner{
+          avatarUrl
+          url
+        }
+        stargazers{
+          totalCount
+        }
+        name
+        releases(last:1){
+          edges{
+            node{
+              tag{
+                name
+              }
+              url
+              updatedAt
+            }
+          }
+        }
+      }
+  }`;
+};
 export const CONSTANT_OTHER = {
   "webpack":"webpack",
   "electron":"electron",
--- whats-new/utils/axios.js
+++ whats-new/utils/axios.js
@@ -1,38 +1,10 @@
-import {CONSTANT_URL,CONSTANT_TOKEN} from '../constant/constant'
+import {CONSTANT_URL,CONSTANT_TOKEN,CONSTANT_QueryInfoWithOwner,CONSTANT_QueryInfoRepository} from '../constant/constant'
 import axios from 'axios'
 
 // 分成两种请求方式是因为 QUERY_WITH_OWNER 需要内部员工的权限才能获取到信息
 // 而 QUERY_REPOSITORIES 不需要内部员工的权限
 export const  QueryInfoWithOwner = (owner,num,success,error)=>{
-  let QUERY_WITH_OWNER =
-    `query {
-      repositoryOwner(login: "${owner}") {
-      avatarUrl
-      url
-      repositories(orderBy: {field: STARGAZERS, direction: DESC}, first: ${num}) {
-        totalCount
-        edges {
-          node {
-            name
-            stargazers {
-              totalCount
-            }
-            releases(last: 1) {
-              edges {
-                node {
-                  tag {
-                    name
-                  }
-                  url
-                  updatedAt
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }`;
+  let QUERY_WITH_OWNER = CONSTANT_QueryInfoWithOwner(owner,num);
   axios.post(CONSTANT_URL,{query:QUERY_WITH_OWNER},{headers: { Authorization: "bearer " + CONSTANT_TOKEN }})
     .then(res => {
       if(res.errors){
@@ -49,30 +21,7 @@ export const  QueryInfoWithOwner = (owner,num,success,error)=>{
     .catch(err =>error(err))
 };
 export const QueryInfoRepository = (owner,repository,success,error)=>{
-  let QUERY_REPOSITORIES =
-    `query {
-      repository(owner:"${owner}",name:"${repository}"){
-        owner{
-          avatarUrl
-          url
-        }
-        stargazers{
-          totalCount
-        }
-        name
-        releases(last:1){
-          edges{
-            node{
-              tag{
-                name
-              }
-              url
-              updatedAt
-            }
-          }
-        }
-      }
-  }`;
+  let QUERY_REPOSITORIES =CONSTANT_QueryInfoRepository(owner,repository);
   axios.post(CONSTANT_URL,{query:QUERY_REPOSITORIES},{headers: { Authorization: "bearer " + CONSTANT_TOKEN }})
     .then(res =>{
       if(res.errors){
